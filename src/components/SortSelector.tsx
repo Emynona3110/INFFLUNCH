769c3f4886da6c7055ea3d31602932b73f78e6e7
Lucas Lambrechts <lambrechtslucas1@gmail.com>
2025-04-25 20:48:30 +0200

    Ajout du composant SortSelector pour permettre le tri des éléments par pertinence, avis, note ou date d'ajout.

---
+++ src/components/SortSelector.tsx
@@ -0,0 +1,51 @@
+import { Menu, MenuButton, Button, MenuList, MenuItem } from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
+
+export type SortOrder = "" | "reviews" | "rating" | "date";
+
+interface SortSelectorProps {
+  onSelectSortOrder: (sortOrder: SortOrder) => void;
+  selectedSortOrder: SortOrder;
+}
+
+const SortSelector = ({
+  onSelectSortOrder,
+  selectedSortOrder,
+}: SortSelectorProps) => {
+  const sortOrders = [
+    { value: "", label: "Pertinence" },
+    { value: "reviews", label: "Avis" },
+    { value: "rating", label: "Note" },
+    { value: "date", label: "Date d'ajout" },
+  ];
+
+  const selectedSortOrderLabel =
+    sortOrders.find((order) => order.value === selectedSortOrder)?.label ??
+    "Relevance";
+
+  return (
+    <Menu>
+      <MenuButton
+        as={Button}
+        rightIcon={<BsChevronDown />}
+        width="225px"
+        textAlign="left"
+      >
+        Trier par : {selectedSortOrderLabel}
+      </MenuButton>
+
+      <MenuList>
+        {sortOrders.map((order) => (
+          <MenuItem
+            key={order.value}
+            onClick={() => onSelectSortOrder(order.value as SortOrder)}
+          >
+            {order.label}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default SortSelector;
