31e6a3ca6ab6a765c46ab1c35661c3b2d45be73d
Lucas Lambrechts <lambrechtslucas1@gmail.com>
2025-05-05 15:26:00 +0200

    Ajout des slugs pour les restaurants et ajout du restaurant Oyama Sushi dans la liste.

--- src/data/restaurants.ts
+++ src/data/restaurants.ts
@@ -2,6 +2,7 @@ export default [
   {
     id: 1,
     name: "L'Atelier du Naan",
+    slug: "latelier-du-naan",
     image:
       "https://tb-static.uber.com/prod/image-proc/processed_images/d8be66d6e29020ddc4b6211731cab5d6/891233042e9ada9873fc6d9c4e835eac.jpeg",
     rating: 3.9,
@@ -13,6 +14,7 @@ export default [
   {
     id: 2,
     name: "Bill's Burger",
+    slug: "bills-burger",
     image:
       "https://bills-burger.fr/_next/image?url=%2Fimg%2Faccueil%2Fimg6.jpg&w=3840&q=75",
     rating: 3.2,
@@ -24,6 +26,7 @@ export default [
   {
     id: 3,
     name: "Authentic Bento",
+    slug: "authentic-bento",
     image:
       "https://tb-static.uber.com/prod/image-proc/processed_images/f5dee66c986bef84a8b1f740376c9f9d/db809eadd12d21eb61044e0f3bf7c9b7.jpeg",
     rating: 5.0,
@@ -35,6 +38,7 @@ export default [
   {
     id: 4,
     name: "O' Five Pizza",
+    slug: "o-five-pizza",
     image:
       "https://rs-menus-api.roocdn.com/images/111864dd-4251-4fd2-a654-df0c89424495/image.jpeg",
     rating: null,
@@ -46,6 +50,7 @@ export default [
   {
     id: 5,
     name: "Pousses",
+    slug: "pousses",
     image:
       "https://rs-menus-api.roocdn.com/images/939426e4-1908-4c43-a2fa-3463eee51af0/image.jpeg",
     rating: null,
@@ -53,5 +58,17 @@ export default [
     badges: ["Option Végétarienne", "TooGoodToGo"],
     reviews: 0,
     address: "14 Rue de Lagny, 93100 Montreuil",
+  },
+  {
+    id: 6,
+    name: "Oyama Sushi",
+    slug: "oyama-sushi",
+    image:
+      "https://res.cloudinary.com/tf-lab/image/upload/w_664,c_fill,g_auto,q_auto,f_auto/restaurant/cb09892a-50fb-4fdb-8281-a0641a133f59/e11a0ccf-9cd8-4650-9199-39b32dbe41ab.jpg",
+    rating: 3.6,
+    tags: ["Japonais", "Sushi"],
+    badges: ["Option Végétarienne"],
+    reviews: 1,
+    address: "42 Rue de Lagny, 93100 Montreuil",
   },
 ];
