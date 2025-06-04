b72e2a8219516a6d93e68d370123f90c761e8189
Lucas Lambrechts <lambrechtslucas1@gmail.com>
2025-06-04 15:17:44 +0200

    Refactor le composant RestaurantCardSkeleton en RestaurantCard et améliore la structure du code avec des éléments de chargement supplémentaires.

--- src/components/RestaurantCardSkeleton.tsx
+++ src/components/RestaurantCardSkeleton.tsx
@@ -1,14 +1,70 @@
-import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+import {
+  Box,
+  Card,
+  CardBody,
+  HStack,
+  Text,
+  VStack,
+  useColorModeValue,
+  Skeleton,
+  SkeletonText,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
+import RestaurantRating from "./StarRating";
 
-const RestaurantCardSkeleton = () => {
+const invisibleChar = "‎ ";
+
+const tagPlaceholder = (n: number) => {
+  return invisibleChar.repeat(n);
+};
+
+const skeletonTags = [
+  tagPlaceholder(10),
+  tagPlaceholder(15),
+  tagPlaceholder(8),
+];
+
+const RestaurantCard = () => {
   return (
-    <Card width={"100%"}>
-      <Skeleton height={"200px"} />
+    <Card borderRadius="10" overflow="hidden" width="100%">
+      <Skeleton
+        height="200px"
+        overflow="hidden"
+        position="relative"
+        fadeDuration={1}
+      />
+
       <CardBody>
-        <SkeletonText />
+        <VStack alignItems="left" spacing={2}>
+          <SkeletonText noOfLines={1} skeletonHeight="5" fadeDuration={1} />
+
+          <HStack
+            verticalAlign="middle"
+            color={useColorModeValue("gray.500", "gray.400")}
+          >
+            <RestaurantRating rating={0} />
+            <SkeletonText fontSize="md" fadeDuration={1} />
+          </HStack>
+
+          <Wrap spacing={2} marginTop={2}>
+            {skeletonTags.map((tag, index) => (
+              <WrapItem key={index}>
+                <Box
+                  paddingX={3}
+                  paddingY={1}
+                  borderRadius="md"
+                  backgroundColor={useColorModeValue("gray.100", "gray.600")}
+                >
+                  <Text>{tag}</Text>
+                </Box>
+              </WrapItem>
+            ))}
+          </Wrap>
+        </VStack>
       </CardBody>
     </Card>
   );
 };
 
-export default RestaurantCardSkeleton;
+export default RestaurantCard;
