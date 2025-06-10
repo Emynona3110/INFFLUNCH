5d7a05943734b12fb57ca0e8676a7afe8ccf466e
Lucas Lambrechts <lambrechtslucas1@gmail.com>
2025-06-10 17:07:34 +0200

    Réorganisation de la fonction slugify : ajustement de la logique de suppression des apostrophes et ajout de la suppression des articles définis en début de chaîne.

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,8 +1,10 @@
 export const slugify = (label: string): string =>
   label
     .toLowerCase()
-    .replace(/\b\p{L}'/gu, "") // Supprime "l'", "d'", etc.
+    .replace(/^(la|le|les|the)\s+/i, "") // Supprime "la ", "le ", "les ", "the " en début
+    .replace(/^\p{L}'/u, "") // Supprime "l'", "d'", etc. uniquement en tout début
     .normalize("NFD")
     .replace(/\p{Diacritic}/gu, "")
+    .replace(/'/g, "") // Supprime toutes les apostrophes restantes
     .replace(/\s+/g, "-")
-    .replace(/[^\w-]/g, "");
+    .replace(/[^\w-]/g, ""); // Supprime les autres caractères non autorisés
